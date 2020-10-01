9f4688c150fabaddb333b5d596cb913f21c2523e
Sharma Pandyala Rakesh (IFIN IT DSA IM EE) <PandyalaRakesh.Sharma-EE@infineon.com>
2020-10-01 11:31:12 +0530

    initial commit

---
+++ blog/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+# Create your tests here.
+#Register
+#Login
+#Update Your profile
+#Create a Post
+#Update a Post
+#delete a Post
+#Logout
+
+#After loggin Out perform below actions
+#Try to access access profile page manually /profile
+
+#Try to create a new post
+
+#Update a New Post
+
+#Again After Loggin in try to update others Post
+
